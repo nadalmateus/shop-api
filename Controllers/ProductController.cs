63a15b5d674544c7a12c19147363533f13b3ad0e
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-03 22:50:11 -0300

    Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+namespace Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using ShopAPI.Data;
+    using ShopAPI.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(int id, [FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory(int id, [FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().Where(x => x.CategoryId == id).ToListAsync();
+            return Ok(products);
+        }
+    }
+}
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace ShopAPI.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-        public DbSet<Product> Produtcs { get; set; }
+        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
     }
